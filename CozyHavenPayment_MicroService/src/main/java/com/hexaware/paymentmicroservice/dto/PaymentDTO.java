28b171768ad3c681d379cb903fd545b332bf83b8
ramya300504 <152589643+ramya300504@users.noreply.github.com>
2025-06-01 23:04:25 +0530

    Payment Microservice

---
+++ CozyHavenPayment_MicroService/src/main/java/com/hexaware/paymentmicroservice/dto/PaymentDTO.java
@@ -0,0 +1,58 @@
+package com.hexaware.paymentmicroservice.dto;
+
+import java.time.LocalDateTime;
+
+public class PaymentDTO {
+
+	private Integer paymentId;
+	
+	private LocalDateTime paymentDate;
+	private double amount;
+	private String paymentMethod;
+	private String paymentStatus;
+	
+	
+	public PaymentDTO(Integer paymentId,LocalDateTime paymentDate, double amount, String paymentMethod, String paymentStatus) {
+		super();
+		this.paymentId=paymentId;
+		this.paymentDate = paymentDate;
+		this.amount = amount;
+		this.paymentMethod = paymentMethod;
+		this.paymentStatus = paymentStatus;
+	}
+	
+	public LocalDateTime getPaymentDate() {
+		return paymentDate;
+	}
+	public void setPaymentDate(LocalDateTime paymentDate) {
+		this.paymentDate = paymentDate;
+	}
+	public double getAmount() {
+		return amount;
+	}
+	public void setAmount(double amount) {
+		this.amount = amount;
+	}
+	public String getPaymentMethod() {
+		return paymentMethod;
+	}
+	public void setPaymentMethod(String paymentMethod) {
+		this.paymentMethod = paymentMethod;
+	}
+	public String getPaymentStatus() {
+		return paymentStatus;
+	}
+	public void setPaymentStatus(String paymentStatus) {
+		this.paymentStatus = paymentStatus;
+	}
+	public Integer getPaymentId() {
+		return paymentId;
+	}
+
+
+	public void setPaymentId(Integer paymentId) {
+		this.paymentId = paymentId;
+	}
+	
+	
+}
